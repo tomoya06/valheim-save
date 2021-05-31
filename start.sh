fe164ff6e38b3ef9facbd7f04c37f66a43ab3da8
tomoya00 <peng4766@gmail.com>
2021-06-01 04:00:04 +0800

    auto save at 2021-06-01 04:00:04+0800

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 docker run -d \
-    --name valheim-server \
-    --cap-add=sys_nice \
-    --stop-timeout 120 \
-    -p 2456-2457:2456-2457/udp \
-    -v $HOME/valheim-server/config:/config \
-    -v $HOME/valheim-server/data:/opt/valheim \
-    -e SERVER_NAME="Namwaa Online" \
-    -e WORLD_NAME="maazeon" \
-    -e SERVER_PASS="476612" \
-    -e BACKUPS_CRON="*/20 * * * *" \
-    -e BACKUPS_MAX_COUNT="5" \
-    lloesche/valheim-server
+	--name valheim-server \
+	--cap-add=sys_nice \
+	--stop-timeout 120 \
+	-p 2456-2457:2456-2457/udp \
+	-v $HOME/valheim-server/config:/config \
+	-v $HOME/valheim-server/data:/opt/valheim \
+	-e SERVER_NAME="Namwaa Online" \
+	-e WORLD_NAME="maazeon" \
+	-e SERVER_PASS="476612" \
+	-e BACKUPS_CRON="*/30 * * * *" \
+	-e BACKUPS_MAX_COUNT="5" \
+	lloesche/valheim-server
